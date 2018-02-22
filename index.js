ad15258441fa58d69e856db7c8601eae90ec3e6d
Mike Holzbach <maholzbach@gmail.com>
2018-02-22 12:19:05 -0500

    Add loader for UX

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const calculateResults = e => {
-  e.preventDefault();
-
+const calculateResults = () => {
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
   const years = document.getElementById("years");
@@ -18,12 +16,18 @@ const calculateResults = e => {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Check your numbers.");
   }
 };
 
 const showError = error => {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "none";
+
   const errorDiv = document.createElement("div");
   errorDiv.className = "alert alert-danger";
   errorDiv.appendChild(document.createTextNode(error));
@@ -40,6 +44,11 @@ const clearError = () => {
   document.querySelector(".alert").remove();
 };
 
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", e => {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
